f4d239e07cfb88d4fa8280f7c9c50f3fdb7f20f1
ducduy1102 <ducduy1110u@gmail.com>
2024-04-02 17:02:18 +0700

    UI SignUp #2

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -15,6 +15,47 @@ const SignUpPageStyle = styled.div`
     font-size: 40px;
     font-style: bold;
   }
+
+  .field {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    row-gap: 20px;
+  }
+
+  .label {
+    color: ${(props) => props.theme.grayDark};
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  .input {
+    width: 100%;
+    padding: 20px;
+    background-color: ${(props) => props.theme.grayLight};
+    border-radius: 8px;
+    font-weight: 500;
+    transition: all 0.2s linear;
+    border: 1px solid transparent;
+  }
+
+  input:focus {
+    background-color: white;
+    border-color: ${(props) => props.theme.primary};
+  }
+
+  .input::-webkit-input-placeholder {
+    color: "#84878b";
+  }
+
+  .input::-moz-input-placeholder {
+    color: "#84878b";
+  }
+
+  .form {
+    max-width: 600px;
+    margin: 0 auto;
+  }
 `;
 
 const SignUpPage = () => {
@@ -23,6 +64,19 @@ const SignUpPage = () => {
       <div className="container">
         <img srcSet="./logo.png 2x" alt="monkey-blogging" className="logo" />
         <h1 className="heading">Monkey Blogging</h1>
+        <form className="form">
+          <div className="field">
+            <label htmlFor="fullname" className="label">
+              Fullname
+            </label>
+            <input
+              id="fullname"
+              type="text"
+              className="input"
+              placeholder="Enter your fullname"
+            />
+          </div>
+        </form>
       </div>
     </SignUpPageStyle>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const theme = {
   primary: "#2ebac1",
+  grayDark: "#292d32",
+  grayLight: "#e7ecf3",
 };
