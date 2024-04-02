30dcc08d84263a8bf622d319ebb1fbb8ca9985d2
ducduy1102 <ducduy1110u@gmail.com>
2024-04-02 16:29:54 +0700

    Setup routes, create auth-context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Routes } from "react-router-dom";
+import { AuthProvider, useAuth } from "./contexts/auth-context";
+
 function App() {
-  return <div></div>;
+  // const {userInfo} = useAuth();
+  return (
+    <div>
+      <AuthProvider>
+        <Routes></Routes>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App;
