3455699df07355af3f9bf0383e3d6c949352d720
ducduy1102 <ducduy1110u@gmail.com>
2024-04-06 00:35:46 +0700

    UI Blog header #1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,20 @@
+import Header from "components/layout/Header";
 import { auth } from "firebase-app/firebase-config";
 import { signOut } from "firebase/auth";
 import React from "react";
+import styled from "styled-components";
+
+const HomePageStyles = styled.div``;
 
 const HomePage = () => {
   const handleSignOut = () => {
     signOut(auth);
   };
-  return <button onClick={handleSignOut}>Sign out</button>;
+  return (
+    <HomePageStyles>
+      <Header></Header>
+    </HomePageStyles>
+  );
 };
 
 export default HomePage;
