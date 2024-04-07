14d6e849ffae791054346b0714ecb689da79a35c
ducduy1102 <ducduy1110u@gmail.com>
2024-04-08 02:19:22 +0700

    UI for module Post

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,8 @@ import Layout from "components/layout/Layout";
 import { auth } from "firebase-app/firebase-config";
 import { signOut } from "firebase/auth";
 import HomeBanner from "module/home/HomeBanner";
+import HomeFeature from "module/home/HomeFeature";
+import HomeNewest from "module/home/HomeNewest";
 import React from "react";
 import styled from "styled-components";
 
@@ -14,9 +16,10 @@ const HomePage = () => {
   // };
   return (
     <HomePageStyles>
-      {/* <Header></Header> */}
       <Layout>
         <HomeBanner></HomeBanner>
+        <HomeFeature></HomeFeature>
+        <HomeNewest></HomeNewest>
       </Layout>
     </HomePageStyles>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,6 @@ export const theme = {
   secondary: "#a4d96c",
   grayDark: "#292d32",
   grayLight: "#e7ecf3",
+  tertiary: "#3A1097",
+  accent: "#00D1ED",
 };
