d93319f7a1b8c3081c9d9c730c62821de0a33de5
ducduy1102 <ducduy1110u@gmail.com>
2024-04-08 22:59:26 +0700

    Install tailwindcss, UI for PostDetailsPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "pages/SignInPage";
 import HomePage from "pages/HomePage";
 import NotFoundPage from "pages/NotFoundPage";
+import PostDetailsPage from "pages/PostDetailsPage";
 
 function App() {
   // const {userInfo} = useAuth();
@@ -15,6 +16,10 @@ function App() {
           <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
           <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
           <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+          <Route
+            path="/:slug"
+            element={<PostDetailsPage></PostDetailsPage>}
+          ></Route>
         </Routes>
       </AuthProvider>
     </div>
