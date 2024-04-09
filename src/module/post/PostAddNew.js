730d4b960c9113374bd48530f692effad0031ece
ducduy1102 <ducduy1110u@gmail.com>
2024-04-09 17:36:11 +0700

    AddPost - Toggle HotFeature function

--- src/module/post/PostAddNew.js
+++ src/module/post/PostAddNew.js
@@ -13,6 +13,7 @@ import ImageUpload from "components/image/ImageUpload";
 // import { addDoc, collection } from "firebase/firestore";
 // import { db } from "firebase-app/firebase-config";
 import useFirebaseImage from "hooks/useFirebaseImage";
+import Toggle from "components/toggle/Toggle";
 
 const PostAddNewStyles = styled.div``;
 
@@ -24,10 +25,12 @@ const PostAddNew = () => {
       slug: "",
       status: 2,
       category: "",
+      hot: false,
     },
   });
   const watchStatus = watch("status");
-  const watchCategory = watch("category");
+  const watchHot = watch("hot");
+  // const watchCategory = watch("category");
 
   const addPostHandler = async (values) => {
     const cloneValues = { ...values };
@@ -41,69 +44,6 @@ const PostAddNew = () => {
     // })
     // handleUploadImage(cloneValues.image);
   };
-  // const [progress, setProgress] = useState(0);
-  // const [image, setImage] = useState("");
-
-  // const handleUploadImage = (file) => {
-  //   // Upload file and metadata to the object 'images/mountains.jpg'
-  //   const storageRef = ref(storage, "images/" + file.name);
-  //   const uploadTask = uploadBytesResumable(storageRef, file);
-
-  //   // Listen for state changes, errors, and completion of the upload.
-  //   uploadTask.on(
-  //     "state_changed",
-  //     (snapshot) => {
-  //       const progressPercent =
-  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  //       // console.log("Upload is " + progress + "% done");
-  //       setProgress(progressPercent);
-  //       switch (snapshot.state) {
-  //         case "paused":
-  //           console.log("Upload is paused");
-  //           break;
-  //         case "running":
-  //           console.log("Upload is running");
-  //           break;
-  //         default:
-  //           console.log("Nothing at all");
-  //       }
-  //     },
-  //     (error) => {
-  //       console.log("Error");
-  //     },
-  //     () => {
-  //       // Upload completed successfully, now we can get the download URL
-  //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-  //         console.log("File available at", downloadURL);
-  //         setImage(downloadURL);
-  //       });
-  //     }
-  //   );
-  // };
-
-  // const onSelectImage = (e) => {
-  //   console.log(e.target.files);
-  //   const file = e.target.files[0];
-  //   if (!file) return;
-  //   setValue("image_name", file.name);
-  //   handleUploadImage(file);
-  // };
-
-  // const handleDeleteImage = () => {
-  //   const imageRef = ref(storage, "images/" + getValues("image_name"));
-
-  //   deleteObject(imageRef)
-  //     .then(() => {
-  //       // File deleted successfully
-  //       console.log("Remove image successfully");
-  //       setImage("");
-  //       setProgress(0);
-  //     })
-  //     .catch((error) => {
-  //       // Uh-oh, an error occurred!
-  //       console.log("Can't remove image");
-  //     });
-  // };
 
   const { image, progress, handleSelectImage, handleDeleteImage } =
     useFirebaseImage(setValue, getValues);
@@ -134,8 +74,6 @@ const PostAddNew = () => {
         <div className="grid grid-cols-2 mb-10 gap-x-10">
           <Field>
             <Label>Image</Label>
-            {/* <Input control={control} placeholder="Find the 
-            author"></Input> */}
             <ImageUpload
               onChange={handleSelectImage}
               handleDeleteImage={handleDeleteImage}
@@ -183,14 +121,18 @@ const PostAddNew = () => {
         </div>
         <div className="grid grid-cols-2 mb-10 gap-x-10">
           <Field>
-            <Label>Category</Label>
-            <Dropdown>
+            <Label>Feature Post</Label>
+            <Toggle
+              on={watchHot === true}
+              onClick={() => setValue("hot", !watchHot)}
+            ></Toggle>
+            {/* <Dropdown>
               <Dropdown.Option>Knowledge</Dropdown.Option>
               <Dropdown.Option>Blockchain</Dropdown.Option>
               <Dropdown.Option>Setup</Dropdown.Option>
               <Dropdown.Option>Nature</Dropdown.Option>
               <Dropdown.Option>Developer</Dropdown.Option>
-            </Dropdown>
+            </Dropdown> */}
           </Field>
           <Field></Field>
         </div>
