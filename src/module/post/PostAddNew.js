c6d86ce9e0888b0aa30e2f2f23d09fa76f56dace
ducduy1102 <ducduy1110u@gmail.com>
2024-04-09 11:17:04 +0700

    AddPost - Upload Image

--- src/module/post/PostAddNew.js
+++ src/module/post/PostAddNew.js
@@ -9,6 +9,14 @@ import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import slugify from "slugify";
 import { postStatus } from "utils/constants";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+const storage = getStorage();
 
 const PostAddNewStyles = styled.div``;
 
@@ -24,20 +32,63 @@ const PostAddNew = () => {
   });
   const watchStatus = watch("status");
   const watchCategory = watch("category");
-  // console.log("PostAddNew ~ watchCategory", watchCategory);
+
   const addPostHandler = async (values) => {
     const cloneValues = { ...values };
     // case enter vietnamese
     cloneValues.slug = slugify(values.slug || values.title);
-    // console.log(cloneValues);
     cloneValues.status = Number(values.status);
     console.log(cloneValues);
+    // handleUploadImage(cloneValues.image);
   };
+
+  const handleUploadImage = (file) => {
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            console.log("Nothing at all");
+        }
+      },
+      (error) => {
+        console.log("Error");
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+  };
+
+  const onSelectImage = (e) => {
+    console.log(e.target.files);
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image", file);
+  };
+
   return (
     <PostAddNewStyles>
       <h1 className="dashboard-heading">Add new post</h1>
       <form onSubmit={handleSubmit(addPostHandler)}>
-        <div className="grid grid-cols-2 gap-x-10 mb-10">
+        <div className="grid grid-cols-2 mb-10 gap-x-10">
           <Field>
             <Label>Title</Label>
             <Input
@@ -56,7 +107,13 @@ const PostAddNew = () => {
             ></Input>
           </Field>
         </div>
-        <div className="grid grid-cols-2 gap-x-10 mb-10">
+        <div className="grid grid-cols-2 mb-10 gap-x-10">
+          <Field>
+            <Label>Image</Label>
+            {/* <Input control={control} placeholder="Find the 
+            author"></Input> */}
+            <input type="file" name="image" onChange={onSelectImage} />
+          </Field>
           <Field>
             <Label>Status</Label>
             <div className="flex items-center gap-x-5">
@@ -89,12 +146,12 @@ const PostAddNew = () => {
               </Radio>
             </div>
           </Field>
-          <Field>
+          {/* <Field>
             <Label>Author</Label>
             <Input control={control} placeholder="Find the author"></Input>
-          </Field>
+          </Field> */}
         </div>
-        <div className="grid grid-cols-2 gap-x-10 mb-10">
+        <div className="grid grid-cols-2 mb-10 gap-x-10">
           <Field>
             <Label>Category</Label>
             <Dropdown>
