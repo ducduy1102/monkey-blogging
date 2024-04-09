7142385a2c41a75a51e2568a3bf2e862e0304abe
ducduy1102 <ducduy1110u@gmail.com>
2024-04-09 22:18:59 +0700

    Query & show Post feature item #1

--- src/module/home/HomeFeature.js
+++ src/module/home/HomeFeature.js
@@ -1,18 +1,49 @@
 import Heading from "components/layout/Heading";
+import { db } from "firebase-app/firebase-config";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import PostFeatureItem from "module/post/PostFeatureItem";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 const HomeFeatureStyles = styled.div``;
 
 const HomeFeature = () => {
+  const [posts, setPost] = useState([]);
+  useEffect(() => {
+    const colRef = collection(db, "posts");
+    const queries = query(
+      colRef,
+      where("status", "==", 1),
+      where("hot", "==", true),
+      limit(3)
+    );
+    onSnapshot(queries, (snapshot) => {
+      // console.log(snapshot);
+      const results = [];
+      snapshot.forEach((doc) => {
+        // console.log(item.data());
+        results.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setPost(results);
+    });
+  }, []);
+  if (posts.length <= 0) return null;
   return (
     <HomeFeatureStyles className="home-block">
       <div className="container">
         <Heading>Bài viết nổi bật</Heading>
         <div className="grid-layout">
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
-          <PostFeatureItem></PostFeatureItem>
+          {posts.map((post) => (
+            <PostFeatureItem key={post.id} data={post}></PostFeatureItem>
+          ))}
         </div>
       </div>
     </HomeFeatureStyles>
